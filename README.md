478e341b6f7a998221ade0fc02576d078ad41f19
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2022-09-10 16:57:02 +0200

    feat: setup express api

---
+++ README.md
@@ -0,0 +1,14 @@
+# Investment dashboard API
+
+[![Netlify
+Status](https://api.netlify.com/api/v1/badges/9aaef7de-1e5d-4fda-bc39-faa10a68b35b/deploy-status)](https://app.netlify.com/sites/netlify-express/deploys)
+
+Stack:
+- [Express.js](https://expressjs.com/)
+- [Prisma](https://www.prisma.io/)
+
+Database: [mySQL on PlanetScale](https://planetscale.com/)
+
+## Setup
+
+Setup `DATABASE_URL` in `.env` file with database connection string
---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+const prisma = new PrismaClient();
+const PORT = 3001;
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Investment dashboard api!</h1>');
+  res.end();
+});
+
+app.get('/transaction', async (req, res) => {
+  const transactions = await prisma.transaction.findMany();
+  res.json(transactions);
+});
+
+app.post(`/post`, async (req, res) => {
+  const { date, amount, price, currency, assetName, category, portfolio } = req.body
+  const result = await prisma.transaction.create({
+    data: {
+      date,
+      amount,
+      price,
+      currency,
+      name: assetName,
+      category,
+      portfolio
+    },
+  })
+  res.json(result)
+});
+
+const server = app.listen(PORT, () =>
+  console.log(`Server ready on port ${PORT}`),
+)
